26d99e56a5c27dc6d728c97e327206cb8f300865
micheni14 <lewismichen42@gmail.com>
2023-05-11 12:18:49 +0300

    deleted matchcenter routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,7 @@ import Mens from "./components/team/Mens";
 import Official from "./components/team/Official";
 import Sponsors from "./components/Sponsors";
 import Footer from "./components/Footer";
-import Gallery from "./components/Gallery";
-import Matchcenter from "./components/matchcenter/Matchcenter";
 
-import Results from "./components/matchcenter/Results";
-import leagueTable from "./components/matchcenter/LeagueTable";
-import LeagueTable from "./components/matchcenter/LeagueTable";
 import Navbar from "./components/Navbar";
 import Contact from "./components/Contact";
 
@@ -33,14 +28,7 @@ function App() {
           </Route>
         </Routes>
         <Sponsors />
-        <Gallery />
-        {/* <Routes>
-          <Route path="/" element={<Matchcenter />}>
-            <Route index element={<Fixtures />} />
-            <Route path="leaguetable" element={<LeagueTable />} />
-            <Route path="results" element={<Results />} />
-          </Route>
-        </Routes> */}
+        <Contact />
         <Footer />
       </div>
     </Router>
